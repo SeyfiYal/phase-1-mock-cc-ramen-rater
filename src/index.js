47b64dae566a57f73f502c10e63f45de6acb14c3
Seyfican Yalcindag <+>
2023-02-08 19:20:35 +0300

    MockChallange

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-// write your code here
+
+
+
+
+
+
+const ramenContainer = document.getElementById("ramen-menu");
+const ramenDetailImage = document.querySelector(".detail-image")
+const ramenName = document.querySelector(".name")
+const ramenRestaurant = document.querySelector(".restaurant")
+
+fetch("http://localhost:3000/ramens")
+    .then((r) => r.json() )
+    .then((json) => loadRamens(json))
+    
+
+    function loadRamens(json){
+
+        json.forEach(makeRamen)
+            
+        }
+
+    
+
+    function makeRamen(ramenObject){
+
+        const ramenDiv = document.getElementById("ramen-menu")
+        let ramenCard = document.createElement("div")
+        ramenCard.className = "ramen"
+
+
+    const ramenImg = document.createElement("img")
+    ramenImg.src = ramenObject.image
+    ramenImg.className = "ramen-menu"
+
+    ramenCard.append(ramenImg)
+    }
+
+
+        // const ramenh2 = document.createElement("h2")
+        // ramenh2.textContent = ramenObject.name
